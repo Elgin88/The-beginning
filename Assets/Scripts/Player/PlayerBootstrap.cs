b56ef82204d13e6c1acd955e37ea67851e699204
aartyomkaa <114865637+aartyomkaa@users.noreply.github.com>
2023-12-20 01:04:56 +0800

    Basic movement and Testing

---
+++ Assets/Scripts/Player/PlayerBootstrap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Assets.Scripts.Input;
+
+namespace Assets.Scripts.Player
+{
+    internal class PlayerBootstrap : MonoBehaviour
+    {
+        [SerializeField] private Player _player;
+        [SerializeField] private PlayerInput _input;
+
+        private void Awake()
+        {
+            _input.Init(new PlayerMover(_player, 8, 2));
+        }
+    }
+}
