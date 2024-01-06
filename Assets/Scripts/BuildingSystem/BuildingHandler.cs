8d0992a84cb12a8a9df9f0db4815c2079646445a
DmitriyLosev8 <dmitriylosev8@gmail.com>
2024-01-06 20:40:19 +0300

    input system

---
+++ Assets/Scripts/BuildingSystem/BuildingHandler.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(InputActions))]
+public class BuildingHandler : MonoBehaviour
+{
+    [SerializeField] private Camera _sceneCamera;
+    [SerializeField] private LayerMask _placeToBuild;
+
+    private Vector2 _inputPosition;
+    private Vector3 _lastPosition;
+    private Vector3 _positionToPlace;
+    private InputActions _inputActions;
+    private float OffsetZ = 0.03f;
+    private int _rayLength = 100;
+
+    public event Action OnPlaced;
+
+    private void Awake()
+    {
+        _inputActions = new InputActions();    
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
+    }
+
+    private void Update()
+    {
+        _inputActions.BuildingSystem.PlaceBuilding.performed += OnPlace;
+        _inputPosition = _inputActions.BuildingSystem.DeterminePosition.ReadValue<Vector2>();
+      
+    }
+
+    private void OnPlace(InputAction.CallbackContext context)  
+    {
+        OnPlaced?.Invoke();
+    }
+
+    public Vector3 DetermineSpot()
+    {
+        _positionToPlace = new Vector3(_inputPosition.x, _inputPosition.y, OffsetZ);
+        
+        Ray ray = _sceneCamera.ScreenPointToRay(_positionToPlace);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, _rayLength, _placeToBuild))
+        {
+            _lastPosition = hit.point;
+        }
+
+        return _lastPosition;
+    }
+
+    public bool IsPointerOverUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
+}
---
+++ Assets/Scripts/BuildingSystem/IBuildingInput.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IBuildingInput 
+{
+    public Vector3 Position;
+    
+    
+    public Vector3 DeterminePosition()
+    {      
+        return Position;
+    } 
+
+
+  
+}
