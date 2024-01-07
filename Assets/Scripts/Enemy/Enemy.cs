94178d887462484280307357a64319502c89971a
Aleksey Elgin <elgin-88@mail.ru>
2024-01-07 21:24:37 +0800

    current

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.GameLogic.Damageable;
 using Assets.Scripts.UnitStateMachine;
 using UnityEngine;
 
@@ -6,7 +7,13 @@ namespace Assets.Scripts.Enemy
     [RequireComponent(typeof(StateMachine))]
     [RequireComponent(typeof(NextTargetFinder))]
 
-    internal class Enemy : MonoBehaviour
+    internal class Enemy : MonoBehaviour, IDamageable
     {
+        public Transform Transform => gameObject.transform;
+
+        public void TakeDamage()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
