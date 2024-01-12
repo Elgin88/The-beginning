6562590aaecebd195b066bd88b61c965cbd6e388
DmitriyLosev8 <dmitriylosev8@gmail.com>
2024-01-12 22:44:26 +0300

    Buildings Components

---
+++ Assets/Scripts/BuildingSystem/Buildings/Building.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.GameLogic.Damageable;
+
+public abstract class Building : MonoBehaviour, IDamageable
+{
+    public int Cost;
+    public int Strength;
+    public ParticleSystem EffectOfDestroying;
+    public GameObject PrafabOfruins;
+
+    public Action OnDestroyed;
+
+    public Transform Transform => this.transform;
+
+    public void TakeDamage(int damage)
+    {
+       if(Strength > 0 && damage > 0)
+        {
+            Strength -= damage;
+
+            if(Strength <= 0)
+            {
+                Destroy();
+            }
+        }  
+    }
+
+    protected void Destroy()
+    {
+        Instantiate(EffectOfDestroying, transform.position, Quaternion.identity);
+        Destroy(gameObject);                                                           // здание будет уничтожаться, но на его месте будут оставаться развалины,
+        Instantiate(PrafabOfruins, transform.position, Quaternion.identity);    // чтобы построить на этом месте заного, их надо будет снести через панель управления за деньги
+        OnDestroyed?.Invoke();   
+    }
+}
