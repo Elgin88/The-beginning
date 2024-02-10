8c4b5e78bca50d7910982b3a3091bf43300751d8
DmitriyLosev8 <dmitriylosev8@gmail.com>
2024-02-10 15:27:45 +0300

    Delete unnecessary assets

--- Assets/TerraWorld/Core/Common/Scripts/Unity/Water/GerstnerDisplace.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace TerraUnity.Runtime
-{
-    [ExecuteInEditMode]
-    [RequireComponent(typeof(WaterBase))]
-    public class GerstnerDisplace : WaterDisplace { }
-}
-
--- Assets/TerraWorld/Graphics/Pro/Crepuscular Rays/Readme.txt
+++
@@ -1,1 +0,0 @@
-Source: https://github.com/super-shaman/crepuscular-rays-Unity
