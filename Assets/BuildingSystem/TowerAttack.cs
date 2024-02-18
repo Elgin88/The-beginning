b1c44039e7148430d421f781f8a3aea3704aed15
DmitriyLosev8 <dmitriylosev8@gmail.com>
2024-02-18 18:41:58 +0300

    test

--- Assets/BuildingSystem/TowerAttack.cs
+++ Assets/BuildingSystem/TowerAttack.cs
@@ -12,46 +12,81 @@ public class TowerAttack : MonoBehaviour
 {
     [SerializeField] private Transform _shootPoint;
     [SerializeField] private Arrow _arrowPrefab;
+    //[SerializeField] private Shell _shellPrefab;
     [SerializeField] private float _delayOfShoot;
     [SerializeField] private bool _isPlayerTower;
     [SerializeField] private float _damage;
+    [SerializeField] private LayerMask _layerMask;
 
     private List<IDamageable> _targets = new();
     private IDamageable _target;
     private float _timeBetweenShoots = 5;
     private ArrowsPool _poolOfArrows;
 
+    private string _enemy = "Enemy";
+    private string _playerUnit = "PlayerUnit";
+    private string _currentLayerMask;
+
     private Coroutine _shootCoroutine;
 
     private void Awake()
+    { 
+         _poolOfArrows = new ArrowsPool(_arrowPrefab, _damage);
+    }
+
+    private void SetCurrentLayerMask(IDamageable idamageable)
     {
-        _poolOfArrows = new ArrowsPool(_arrowPrefab, _damage);
+        if (idamageable.IsPlayerObject)
+        {
+            _currentLayerMask = _playerUnit;
+        }
+        else
+        {
+            _currentLayerMask = _enemy;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (_isPlayerTower)
-        {
             if (other.gameObject.TryGetComponent(out IDamageable idamageable))
             {
-                if (!idamageable.IsPlayerObject)
+                SetCurrentLayerMask(idamageable);
+
+                if (_isPlayerTower && _currentLayerMask == _enemy)
                 {
                     StartCoroutine(Shoot(idamageable.Transform));
+                    //InitShell(idamageable);
+                    Debug.Log("Лечу на врага");
                 }
-            }
-        }
-       
-        if (!_isPlayerTower)
-        {
-            if (other.gameObject.TryGetComponent(out IDamageable idamageable))
-            {
-                if (idamageable.IsPlayerObject)
+
+                if (!_isPlayerTower && _currentLayerMask == _playerUnit)
                 {
                     StartCoroutine(Shoot(idamageable.Transform));
-                }
+                    // InitShell(idamageable);
+                     Debug.Log("Лечу на игрока");
+
+                 }
+                
+               // StartCoroutine(Shoot(idamageable.Transform));
             }
-        }
     }
+
+    //private void InitShell(IDamageable idamageable)
+    //{
+    //   Shell shell =  Instantiate(_shellPrefab, _shootPoint.position, Quaternion.identity);
+    //   shell.Init(idamageable.Transform);
+    //}
+
+    //private IEnumerator InitShell2(IDamageable idamageable)
+    //{
+    //    yield return new WaitForSeconds(_delayOfShoot);
+
+    //    Shell shell = Instantiate(_shellPrefab, _shootPoint.position, Quaternion.identity);
+    //    shell.Init(idamageable.Transform);
+    //}
+
+
+
 
     private IEnumerator Shoot(Transform target)
     {
@@ -61,22 +96,20 @@ public class TowerAttack : MonoBehaviour
         arrow.transform.position = _shootPoint.position;
 
 
-       // Arrow arrow = Instantiate(_arrowPrefab);
-       // arrow.gameObject.SetActive(true);
-       // arrow.transform.position = _shootPoint.position;
-       // arrow.Init(_damage);
+        // Arrow arrow = Instantiate(_arrowPrefab);
+        // arrow.gameObject.SetActive(true);
+        // arrow.transform.position = _shootPoint.position;
+        // arrow.Init(_damage);
         arrow.Fly(target);
-       
-        //yield return null;
     }
 
-    private void TryToShot(Transform target)
-    {
-        if (_shootCoroutine != null)
-        {
-            StopCoroutine(_shootCoroutine);
-        }
-       
-        _shootCoroutine = StartCoroutine(Shoot(target));
-    }
+    //private void TryToShot(Transform target)
+    //{
+    //    if (_shootCoroutine != null)
+    //    {
+    //        StopCoroutine(_shootCoroutine);
+    //    }
+
+    //    _shootCoroutine = StartCoroutine(Shoot(target));
+    //}
 }
--- Assets/Scripts/BuildingSystem/Buildings/Tower.cs
+++ Assets/Scripts/BuildingSystem/Buildings/Tower.cs
@@ -8,9 +8,23 @@ namespace Assets.Scripts.BuildingSystem.Buildings
     internal class Tower : Building
     {
 
-        //[SerializeField] private Bow _bow;
+       // [SerializeField] private Bow _bow;
         
         public override bool IsPlayerObject => gameObject;
 
-    }
+        //public void Attack()
+        //{
+        //    if (_bow.CanAttack)
+        //    {
+        //        // _animator.SetAnimatorAttackTrigger(_currentWeapon);
+
+        //        _bow.Attack();
+        //    }
+        //}
+
+        //private void Update()
+        //{
+        //    Attack();
+        //}
+    }  
 }
