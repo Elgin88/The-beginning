54f5dd59572217d3be39a79a6ca2d624fadde4bf
DmitriyLosev8 <dmitriylosev8@gmail.com>
2024-02-20 00:30:40 +0300

    All and Single targets tower attack

--- Assets/BuildingSystem/TowerAttack.cs
+++ Assets/BuildingSystem/TowerAttack.cs
@@ -6,6 +6,7 @@ using Assets.Scripts.PlayerComponents.Weapons;
 using Assets.Scripts.PlayerComponents.Weapons.Bows;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,11 +20,9 @@ public class TowerAttack : MonoBehaviour
     [SerializeField] private float _damage;
     [SerializeField] private LayerMask _targetlayerMask;
 
-    private bool _isTargetNear;
     private List<Transform> _targets = new();
     private Transform _target;
     private ArrowsPool _poolOfArrows;
-    private Coroutine _shootCoroutine;
     private float _currentDelay;
 
     private void Awake()
@@ -31,40 +30,14 @@ public class TowerAttack : MonoBehaviour
          _poolOfArrows = new ArrowsPool(_arrowPrefab, _damage, _targetlayerMask);
     }
 
-    //private void Attack(Transform target)
-    //{
-    //    if (_shootCoroutine != null)
-    //    {
-    //        StopCoroutine(_shootCoroutine);
-    //    }
-
-    //    _shootCoroutine = StartCoroutine(Shoot(target));
-    //}
-
     private void Update()
     {
         if (_targets.Count > 0)
         {
             TryForShoot();
+            CleanTargets(_targets);
         }
     }
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    int mask = 1 << other.gameObject.layer;
-
-    //    _isTargetNear = true;
-    //    Debug.Log(other.gameObject.name);
-
-    //    if (other.gameObject.TryGetComponent(out IDamageable target) && mask == _targetlayerMask)
-    //    {
-    //        _targets.Add(target.Transform);
-
-
-    //        Attack(target.Transform);
-    //    }
-    //}
-
 
     private void TryForShoot()
     {  
@@ -73,75 +46,54 @@ public class TowerAttack : MonoBehaviour
             Shoot();
             _currentDelay = 0;
         }  
+        
         _currentDelay += Time.deltaTime;
-
     }
 
     private void Shoot()
-    {        
-        for(int i = 0; i < _targets.Count; i++)
-            {
-                Arrow arrow = _poolOfArrows.GetArrow();
+    {
+        //for (int i = 0; i < _targets.Count; i++)   //AllTargetsAttack
+        //{
+        //    Arrow arrow = _poolOfArrows.GetArrow();
 
-                arrow.transform.position = _shootPoint.position;
-                arrow.Fly(_targets[i]);
-            }   
+        //    arrow.transform.position = _shootPoint.position;
+        //    arrow.Fly(_targets[i]);
+        //}
+
+       //FirstTargetAttack
+        Arrow arrow = _poolOfArrows.GetArrow();
+        arrow.transform.position = _shootPoint.position;
+        arrow.Fly(_targets.First());
+    }
+
+    private void CleanTargets(List<Transform> targets)
+    {
+        for(int i = 0; i < _targets.Count; i++)
+        {
+            if (targets[i] == null && targets[i].gameObject.activeSelf == false)
+            {
+                _targets.RemoveAt(i);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         int mask = 1 << other.gameObject.layer;
 
-        //_isTargetNear = true;
-        //Debug.Log(other.gameObject.name);
-
         if (other.gameObject.TryGetComponent(out IDamageable target) && mask == _targetlayerMask)
         {
             _targets.Add(target.Transform);
-
-
-           // Attack(target.Transform);
         }
     }
 
-    private void SpotTargets(Transform target)
-    {
-        _targets.Add(target);
-    }
-
-
     private void OnTriggerExit(Collider other)
     {
-
         int mask = 1 << other.gameObject.layer;
 
         if (other.gameObject.TryGetComponent(out IDamageable target) && mask == _targetlayerMask)
         {
             _targets.Remove(target.Transform);
-           // _isTargetNear = false;
         }
-
-
-        //int mask = 1 << other.gameObject.layer;
-
-        //if (other.gameObject.TryGetComponent(out IDamageable target) && mask == _targetlayerMask)
-        //{
-        //    _targets.Remove(target.Transform);
-        //    _isTargetNear = false;
-        //    StopCoroutine(_shootCoroutine);
-        //}
-
     }
-
-    //private IEnumerator Shoot(Transform target)
-    //{
-    //    while (_isTargetNear)
-    //    {
-    //        Arrow arrow = _poolOfArrows.GetArrow();
-
-    //        arrow.transform.position = _shootPoint.position;
-    //        arrow.Fly(target);
-    //        yield return new WaitForSeconds(_delayOfShoot);
-    //    }  
-    //}
 }
