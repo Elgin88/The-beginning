5319740a2b97bcf1defa962afc5cd134554f3d82
DmitriyLosev8 <dmitriylosev8@gmail.com>
2024-03-14 23:21:02 +0300

    PivotAndCameraFollow

--- Assets/Scripts/CameraComponents/TargetFollower.cs
+++ Assets/Scripts/CameraComponents/TargetFollower.cs
@@ -6,25 +6,48 @@ namespace Assets.Scripts.CameraComponents
 {
     internal class TargetFollower : MonoBehaviour
     {
+        [Header("Position")]
+        
+        [SerializeField] private float _offsetPositionY;
+        [SerializeField] private float _offsetPositionX;
+        [SerializeField] private float _offsetPositionZ;
+
+        [Header("Rotation")]
+
+        [SerializeField] private float _offsetRotationY;
+        [SerializeField] private float _offsetRotationX;
+        [SerializeField] private float _offsetRotationZ;
+
         private Player _player;
-
-        private Vector3 _cameraOffset;
-
-        private void Awake()
-        {
-            _cameraOffset = transform.position;
-        }
-
-        private void LateUpdate()
-        {
-            Vector3 newPosition = new Vector3(_player.transform.position.x, 0, _player.transform.position.z) + _cameraOffset;
-            transform.position = newPosition;
-        }
+        private int QuaternionWValue = 15;
 
         [Inject]
         private void Construct(Player player)
         {
             _player = player;
+        }
+       
+        private void Start()
+        {
+            //SetRotation();
+        }
+
+        private void LateUpdate()
+        {
+            SetPosition();
+            SetRotation();
+        }
+
+
+        private void SetPosition()
+        {
+            transform.position = new Vector3(_player.transform.position.x + _offsetPositionX,
+                _player.transform.position.y + _offsetPositionY, _player.transform.position.z + _offsetPositionZ);
+        }
+
+        private void SetRotation()
+        {
+            transform.rotation = new Quaternion(_offsetRotationX, _offsetRotationY, _offsetRotationZ, QuaternionWValue);
         }
     }
 }
