ca841e8f239eae278b773191e5d2685423d473b6
DmitriyLosev8 <dmitriylosev8@gmail.com>
2024-03-17 20:44:30 +0300

    CollidersOnBuildings

--- Assets/Invector-3rdPersonController_LITE/TargetFollowTest.cs
+++ Assets/Invector-3rdPersonController_LITE/TargetFollowTest.cs
@@ -8,22 +8,18 @@ namespace Assets.Scripts.AuxiliaryComponents
     {
        // private Player _player;
 
-       //// [SerializeField] private Transform _target;
-       // [SerializeField] private float _offsetPositionY;
-       // [SerializeField] private float _offsetPositionX;
-       // [SerializeField] private float _offsetPositionZ;
+         [SerializeField] private Transform _target;
+        [SerializeField] private float _offsetPositionY;
+        [SerializeField] private float _offsetPositionX;
+        [SerializeField] private float _offsetPositionZ;
 
 
-       // private void LateUpdate()
-       // {
-       //     transform.position = new Vector3(_player.transform.position.x + _offsetPositionX, 
-       //         _player.transform.position.y + _offsetPositionY, _player.transform.position.z + _offsetPositionZ);
-       // }
+        private void LateUpdate()
+        {
+            transform.position = new Vector3(_target.position.x + _offsetPositionX,
+               _target.position.y + _offsetPositionY, _target.position.z + _offsetPositionZ);
+        }
 
-       // [Inject]
-       // private void Construct(Player player)
-       // {
-       //     _player = player;
-       // }
+       
     }
 }
