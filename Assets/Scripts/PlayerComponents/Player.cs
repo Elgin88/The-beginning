3cc6376805e8fc09b47e6706202808eb770b5855
aartyomkaa <114865637+aartyomkaa@users.noreply.github.com>
2024-03-22 19:00:54 +0300

    fix

--- Assets/Scripts/PlayerComponents/Player.cs
+++ Assets/Scripts/PlayerComponents/Player.cs
@@ -6,13 +6,20 @@ namespace Assets.Scripts.PlayerComponents
 {
     internal class Player : MonoBehaviour, IMoveable
     {
+        private PlayerWallet _wallet;
+
         public float Speed {get; private set;}
 
         public float RotationSpeed {get; private set;}
 
-        public PlayerWallet Wallet => new PlayerWallet();
+        public PlayerWallet Wallet => _wallet;
 
         public Transform Transform => transform;
+
+        private void Start()
+        {
+            _wallet = new PlayerWallet();
+        }
 
         [Inject]
         private void Construct(PlayerData config)
