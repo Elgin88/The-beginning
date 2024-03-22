71800a4114f6584df88487b66bcb911d7c5b6324
Elgin88 <111440347+Elgin88@users.noreply.github.com>
2024-03-22 10:00:36 +0800

    eaa

--- Assets/Scripts/Enemy/EnemyMelee/EnemyMeleeOgreGreen.cs
+++ Assets/Scripts/Enemy/EnemyMelee/EnemyMeleeOgreGreen.cs
@@ -1,6 +1,25 @@
+using Assets.Scripts.BuildingSystem.Buildings;
+using UnityEngine;
+
 namespace Assets.Scripts.Enemy
 {
-    internal class EnemyMeleeOgreGreen : EnemyMelee
+    internal class EnemyMelleeOrcGreen : EnemyMelee
     {
+        [SerializeField] private EnemyNextTargetFinder _enemyNextTargetFinder;
+
+        internal override void InitMainBuilding(MainBuilding mainBuilding)
+        {
+            _enemyNextTargetFinder.InitMainBuilding(mainBuilding);
+        }
+
+        internal override void SetPosition(Vector3 position)
+        {
+            transform.position = position;
+        }
+
+        internal override void SetRotation()
+        {
+            transform.rotation = Quaternion.LookRotation(_enemyNextTargetFinder.CurrentTargetPosition);
+        }
     }
 }
